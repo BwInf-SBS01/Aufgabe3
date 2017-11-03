2a5bd7b5935b054cde9473010a2884582f568ebc
SBSJavatar <31701319+SBSJavatar@users.noreply.github.com>
2017-11-03 17:20:27 +0100

    Add files via upload

--- DarstellungFrame.java
+++ DarstellungFrame.java
@@ -3,6 +3,7 @@ package de.dreiecke_zaehlen.java;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Panel;
 import java.awt.Point;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -13,68 +14,70 @@ import java.io.IOException;
 import java.util.*;
 
 
+
 public class DarstellungFrame extends Frame {
-  
-  private static final long serialVersionUID = 186708090502948842L;
-  private List<Point> punkte = new ArrayList<Point>();
-  int anzahl;
-  
-  public DarstellungFrame(String file) {
-    super();
-    addWindowListener(new WindowAdapter() {
-      
-      @Override
-      public void windowClosing(WindowEvent e) {
-        dispose();
-        System.exit(0);
-        
-      }
-    });
-    setTitle("Test");
-    setBounds(100, 100, 300, 200);
-    setBackground(Color.GRAY);
-    setUndecorated(true);
-    setVisible(true);
-    test(this, file);
-  }
-  
-  
-  public void test(Frame frame, String file) {
-    
-    Graphics g = frame.getGraphics();
-    anzahl = loadPoints(file);
-    paint(g);
-    this.setVisible(true);
-  }
-  @Override
-  public void paint(Graphics g) {
-    g.setColor(Color.red);
-    for (int i = 0; i < anzahl * 2; i = i + 2) {
-      //System.out.println(punkte.get(i)+" to "+punkte.get(i+1));
-      g.drawLine(punkte.get(i).x, punkte.get(i).y, punkte.get(i + 1).x, punkte.get(i + 1).y);
-    }
-  }
-  
-  private int loadPoints(String file) {
-    FileReader fr = null;
-    try {
-      fr = new FileReader(new File("src/" + file));
-      @SuppressWarnings("resource")
-      BufferedReader reader = new BufferedReader(fr);
-      
-      String line;
-      int anzahl = Integer.parseInt(reader.readLine());
-      while ((line = reader.readLine()) != null) {
-        String[] zahlen;
-        zahlen = line.split(" ");
-        punkte.add(new Point((int) Float.parseFloat(zahlen[0]), (int) Float.parseFloat(zahlen[1])));
-        punkte.add(new Point((int) Float.parseFloat(zahlen[2]), (int) Float.parseFloat(zahlen[3])));
-      }
-      return anzahl;
-    } catch (IOException e) {
-      System.err.println("Couldn't load file!");
-      return 0;
-    }
-  }
-  
+
+	private static final long serialVersionUID = 186708090502948842L;
+	private List<Point> punkte = new ArrayList<Point>();
+	int anzahl;
+
+	public DarstellungFrame(String file) {
+		super();
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dispose();
+				System.exit(0);
+
+			}
+		});
+		setTitle("Visualisierung:" + file);
+		setBounds(100, 100, 400, 400);
+		setVisible(true);
+		run(this, file);
+		
+		setVisible(true);
+	}
+
+	private void run(Frame frame, String file) {
+
+		Graphics g = frame.getGraphics();
+		anzahl = loadPoints(file);
+		paint(g);
+		this.setVisible(true);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.setColor(Color.red);
+		for (int i = 0; i < anzahl * 2; i = i + 2) {
+			System.out.println(punkte.get(i) + " to " + punkte.get(i + 1));
+			g.drawLine(punkte.get(i).x + 50, this.getHeight() - punkte.get(i).y - 50, punkte.get(i + 1).x + 50,
+					this.getHeight() - punkte.get(i + 1).y - 50);
+		}
+	}
+
+	private int loadPoints(String file) {
+		FileReader fr = null;
+		try {
+			fr = new FileReader(new File("src/" + file));
+			@SuppressWarnings("resource")
+			BufferedReader reader = new BufferedReader(fr);
+
+			String line;
+			int anzahl = Integer.parseInt(reader.readLine());
+			while ((line = reader.readLine()) != null) {
+				String[] zahlen;
+				zahlen = line.split(" ");
+				punkte.add(new Point((int) Float.parseFloat(zahlen[0]), (int) Float.parseFloat(zahlen[1])));
+				punkte.add(new Point((int) Float.parseFloat(zahlen[2]), (int) Float.parseFloat(zahlen[3])));
+			}
+			return anzahl;
+		} catch (IOException e) {
+			System.err.println("Couldn't load file!");
+			return 0;
+		}
+	}
+
 }
